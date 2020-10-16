5a6ade34eeef55d4734075cf4b3f3af5164ae19b
RAYMOND ALFRED ADANZA (IM-MIS-ISD-OOCLL/MNL) <ADANZRA@OOCL.COM>
2020-10-16 15:19:01 +0800

    [ParkingLot][Raymond] Feature: return no cars when given used ticket

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,10 +1,12 @@
 package com.oocl.cultivation;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ParkingLot {
     private Map<ParkingTicket, Car> ticketCarMap = new HashMap<>();
+    private ArrayList<Car> ticketList = new ArrayList<Car>();
+    private Car car = new Car();
+    private Car ticket = new Car();
 
     public ParkingTicket park(Car car) {
         ParkingTicket ticket = new ParkingTicket();
@@ -13,6 +15,19 @@ public class ParkingLot {
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
-    return ticketCarMap.get(parkingTicket);
+        //System.out.println(ticketCarMap.get(parkingTicket));
+        car = ticketCarMap.get(parkingTicket);
+
+       ticket = ticketList.contains(car) ? null
+               : ticketCarMap.get(parkingTicket);
+
+       if(ticket == null) {
+           return null;
+       }
+       else {
+           ticketList.add(car);
+       }
+
+        return ticket;
     }
 }
