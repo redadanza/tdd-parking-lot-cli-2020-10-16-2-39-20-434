c8e86f607cf04b536fa72a24f183d75063e36965
RAYMOND ALFRED ADANZA (IM-MIS-ISD-OOCLL/MNL) <ADANZRA@OOCL.COM>
2020-10-16 14:17:37 +0800

    [ParkingLot][Raymond] Feature: execute fetch when given a ticket to parking boy

--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -4,14 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
+    private Map<ParkingTicket, Car> ticketCarMap = new HashMap<>();
 
     public ParkingTicket park(Car car) {
         ParkingTicket ticket = new ParkingTicket();
-
+        ticketCarMap.put(ticket,car);
         return ticket;
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
-    return null;
+    return ticketCarMap.get(parkingTicket);
     }
 }
